04c6c5f33eae37dbd5b4a2a1233d769bef04e1c4
HodaJahangiri <hoda.cs87@gmail.com>
2025-11-10 16:31:13 -0800

    Authentication functions done

---
+++ bp_owner.py
@@ -0,0 +1,19 @@
+from models import Owners, session
+
+#View profile function
+#displays the current users info
+def view_owner(current_user):
+    
+
+
+#Update profile function
+#displays current user info
+#allows user to update any of the fields
+#commits changes 
+#shows changes and returns update current_user
+
+#Update profile function
+#Ask user to confirm they want to delete
+#if so delete the current user from the session
+#commits changes 
+#call main() to start the program over
